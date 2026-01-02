e263d649ba012226367c33f4ef716cc2b5bb765a
Phong Vu 2010 <phongvu2010@gmail.com>
2026-01-03 01:17:59 +0700

    master

--- app/modules/users/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from ...core.config import settings
-from .api import auth, users, views
-
-router = APIRouter()
-router.include_router(auth.router, prefix=settings.API_V1_STR)
-router.include_router(users.router, prefix=settings.API_V1_STR)
-router.include_router(views.router)
